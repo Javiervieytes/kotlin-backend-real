7a391dab1e83be5c8a12c047aae3cd7d8019fae5
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-12-04 17:38:35 -0300

    Add usuario relationship to Resena model
    Added a ManyToOne relationship with Usuario.

--- src/main/java/com/kotlin/demo/model/Resena.java
+++ src/main/java/com/kotlin/demo/model/Resena.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +23,8 @@ public class Resena {
     private String title;
     private String description;
     private String categoria;
-}
 
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+}
