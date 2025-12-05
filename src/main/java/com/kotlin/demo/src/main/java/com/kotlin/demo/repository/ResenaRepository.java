bde50ccc1bdf41a2407131e322300f41fa76e311
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-12-05 01:55:40 -0300

    Delete src/main/java/com/kotlin/demo/src directory

--- src/main/java/com/kotlin/demo/src/main/java/com/kotlin/demo/repository/ResenaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kotlin.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kotlin.demo.model.Resena;
-
-public interface ResenaRepository extends JpaRepository<Resena, Long> {
-}
