b6faed9c0dbadd52d344d19ff974b05a6ca5fc24
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-12-05 01:02:41 -0300

    Add findByCorreoAndContrasena method to UsuarioRepository
    Added a method to find a user by email and password.

--- src/main/java/com/kotlin/demo/repository/UsuarioRepository.java
+++ src/main/java/com/kotlin/demo/repository/UsuarioRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kotlin.demo.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+  Optional<Usuario> findByCorreoAndContrasena(String correo, String contrasena);
 }
+
