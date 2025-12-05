09a35c806f352abeda172e9bcbc59322131c7b4b
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-12-05 01:28:56 -0300

    Update UsuarioRepository.java

--- src/main/java/com/kotlin/demo/repository/UsuarioRepository.java
+++ src/main/java/com/kotlin/demo/repository/UsuarioRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kotlin.demo.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-  Optional<Usuario> findByCorreoAndContrasena(String correo, String contrasena);
 }
 
